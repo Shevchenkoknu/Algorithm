bf659bef4f67d9bffe1a69ccc40870290ad117c3
Shevchenkoknu <Shevchenko_Vlada@knu.ua>
2025-05-25 23:51:20 +0300

    Update t03_04_e27.py

--- 3/3.4/t03_04_e27.py
+++ 3/3.4/t03_04_e27.py
@@ -1,14 +1,12 @@
+def linear(heights, a, b):
+    count = 0
+    for h in heights:
+        if a <= h <= b:
+            count += 1
+    return count
+
 n = int(input())
+heights = list(map(int, input().split()))
+a, b = map(int, input().split())
 
-def cyclic(n):
-    bin_str = bin(n)[2:]
-    max = n
-    curr = bin_str
-    for _ in range(len(bin_str)):
-        curr = curr[1:] + curr[0]
-        val = int(curr, 2)
-        if val > max:
-            max = val
-    return max
-
-print(cyclic(n))
+print(count_linear(heights, a, b))
